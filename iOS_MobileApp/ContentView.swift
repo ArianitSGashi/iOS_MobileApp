9cfbe54d3c2c8355ff4c6ee17e1b216df6d77c97
ArditGjyrevci <arianit.gashi9@student.uni-pr.edu>
2024-02-26 13:40:30 +0100

    Initial Commit

---
+++ iOS_MobileApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS_MobileApp
+//
+//  Created by Hakan Shehu on 26.2.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_MobileApp/iOS_MobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_MobileAppApp.swift
+//  iOS_MobileApp
+//
+//  Created by Hakan Shehu on 26.2.24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_MobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
